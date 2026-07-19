5becbbe4b5b84e5da5aa5de7bfd60a811b24dbec
Eswar Balasubramanian <eswarbtechit@gmail.com>
2026-07-19 20:22:28 +0530

    Added web hosting options and github pages config added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.Copy
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -9,6 +11,22 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.sqldelight) apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
+}
+
+tasks.register<Copy>("prepareGithubPages") {
+    group = "deployment"
+    description = "Builds the web app and copies its static assets into webApp/docs for GitHub Pages."
+    dependsOn(":webApp:jsBrowserProductionWebpack")
+
+    from(file("webApp/src/webMain/resources")) {
+        include("**/*")
+    }
+
+    from(file("webApp/build/kotlin-webpack/js/productionExecutable")) {
+        include("*.js", "*.mjs", "*.wasm", "*.css", "*.map", "*.txt")
+    }
+
+    into(file("webApp/docs"))
 }
 
 tasks.register("androidBuild") {
